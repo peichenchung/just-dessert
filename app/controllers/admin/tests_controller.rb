ad5d167c11298b25802faeaaa6314b95c003e216
revichung <s1492e034@gmail.com>
2018-09-08 12:26:27 +0800

    add test index action and view for admin

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,7 @@
+class Admin::TestsController < ApplicationController
+
+  def index #管理員後台首頁
+    
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   
   root "tests#index" #前台首頁
 
+  namespace :admin do
+    root "tests#index" #管理員後台首頁
+  end
+
 end
