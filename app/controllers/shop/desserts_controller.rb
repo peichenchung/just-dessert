899aaa3dbd96ea17451c666a4a53ab405fd762be
revichung <s1492e034@gmail.com>
2018-09-09 17:01:20 +0800

    add dessert new and create actions and view for shop

--- app/controllers/shop/desserts_controller.rb
+++ app/controllers/shop/desserts_controller.rb
@@ -5,4 +5,25 @@ class Shop::DessertsController < ApplicationController
   def index
     
   end
+
+  def new
+    @dessert = Dessert.new
+  end
+
+  def create
+    @dessert = Dessert.new(dessert_params)
+    if @dessert.save
+      flash[:notice] = "甜點上架完成"
+      redirect_to shop_desserts_path
+    else
+      flash.now[:alert] = "甜點上架失敗!!"
+      render :new
+    end
+  end
+
+  private
+
+  def dessert_params
+    params.require(:dessert).permit(:name, :price, :image, :description)
+  end
 end
--- app/models/dessert.rb
+++ app/models/dessert.rb
@@ -1,2 +1,4 @@
 class Dessert < ApplicationRecord
+  validates_presence_of :name
+  mount_uploader :image, ImageUploader #產品(甜點)圖片
 end
