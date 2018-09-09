cc9f334e0286b0e0cf66ac2fab94f4c79c03c29f
revichung <s1492e034@gmail.com>
2018-09-09 17:33:58 +0800

    save shop_id when create new dessert

--- app/controllers/shop/desserts_controller.rb
+++ app/controllers/shop/desserts_controller.rb
@@ -12,6 +12,7 @@ class Shop::DessertsController < ApplicationController
 
   def create
     @dessert = Dessert.new(dessert_params)
+    @dessert.shop_id = current_user.shop.id
     if @dessert.save
       flash[:notice] = "甜點上架完成"
       redirect_to shop_desserts_path
@@ -24,6 +25,6 @@ class Shop::DessertsController < ApplicationController
   private
 
   def dessert_params
-    params.require(:dessert).permit(:name, :price, :image, :description)
+    params.require(:dessert).permit(:name, :price, :image, :description, :shop_id)
   end
 end
