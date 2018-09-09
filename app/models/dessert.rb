1bfd06195b152be7277c60a2fd0edfd32662ee29
revichung <s1492e034@gmail.com>
2018-09-09 17:29:18 +0800

    setup relations between shop and desserts

--- app/models/dessert.rb
+++ app/models/dessert.rb
@@ -1,4 +1,6 @@
 class Dessert < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, ImageUploader #產品(甜點)圖片
+
+  belongs_to :shop
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,4 +2,6 @@ class Shop < ApplicationRecord
   validates_presence_of :shopname
   mount_uploader :logo, LogoUploader #for logo
   mount_uploader :image, ImageUploader #店家形象圖
+
+  has_many :desserts
 end
