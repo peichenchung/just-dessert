9b2006f72f65ca7bf542868f88c9378f50546c7d
revichung <s1492e034@gmail.com>
2018-09-10 20:30:05 +0800

    setup route for user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "tests#index" #前台首頁
 
+  resources :users, only: [:show, :edit, :update]
+
   namespace :admin do
     root "tests#index" #管理員後台首頁
   end
